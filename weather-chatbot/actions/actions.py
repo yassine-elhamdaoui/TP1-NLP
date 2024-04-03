b238bc24f463214f3ad86e04828fac6d50a0b4bb
yassine-elhamdaoui <elhamdaouiy288@gmail.com>
2024-04-03 23:01:41 +0000

    TP4 : adding the chatbot

---
+++ weather-chatbot/actions/actions.py
@@ -0,0 +1,27 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+
+class ActionCheckWeather(Action):
+
+    def name(self)-> Text:
+        return "action_get_weather"
+    
+    def run(self, dispatcher, tracker, domain):
+        api_key = '50abbc34600928d32faf63ba97c672be'
+        loc = tracker.get_slot('location')
+        current = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(loc, api_key)).json()
+        print(current)
+        country = current['sys']['country']
+        city = current['name']
+        condition = current['weather'][0]['main'    ]
+        temperature_c = current['main']['temp']
+        humidity = current['main']['humidity']
+        wind_mph = current['wind']['speed']
+        response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(condition, city, temperature_c, humidity, wind_mph)
+        dispatcher.utter_message(response)
+        return [SlotSet('location', loc)]
